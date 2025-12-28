deb1e54e12d1758db845bd5f0dab853e1818afd1
Sonu Kumar <kumarsonuk430@gaiml.com>
2025-12-28 21:48:11 +0530

    cloudinary & multer setup

---
+++ BackEnd/Project-01/src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    // Upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // File upload successfully
+    console.log("File is uploaded Successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved file as the upload operation got failed
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
