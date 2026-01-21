4ef6037044ad406484fcbf7f84864a8b0adfa8d1
Sonu Kumar <kumarsonuk430@gaiml.com>
2026-01-21 22:56:09 +0530

    new product controller done

--- BackEnd/06.Store API/Server/controllers/product.controler.js
+++ BackEnd/06.Store API/Server/controllers/product.controler.js
@@ -1,3 +1,5 @@
+const Product = require("../models/product.model");
+
 const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: "Products testing route" });
 };
@@ -5,5 +7,31 @@ const getAllProductsStatic = async (req, res) => {
 const getAllProducts = async (req, res) => {
   res.status(200).json({ msg: "Products route" });
 };
+//! New Product created
+const addProducts = async (req, res) => {
+  try {
+    const { name, price } = req.body;
 
-module.exports = { getAllProducts, getAllProductsStatic };
+    if (!name) {
+      return res.status(400).json({ message: "name is required" });
+    }
+    if (!price) {
+      return res.status(400).json({ message: "price is required" });
+    }
+
+    const newProduct = await Product.create(req.body);
+
+    // send response
+
+    res.status(201).json({
+      message: "Product created successfully",
+      productList: newProduct,
+    });
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+
+  // res.status(201).json({ msg: "Product crested" });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic, addProducts };
--- BackEnd/06.Store API/Server/routes/product.route.js
+++ BackEnd/06.Store API/Server/routes/product.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getAllProducts,
   getAllProductsStatic,
+  addProducts,
 } = require("../controllers/product.controler");
 
 const router = express.Router();
 
 router.route("/").get(getAllProducts);
 router.route("/static").get(getAllProductsStatic);
+router.route("/addProduct").post(addProducts);
 
 module.exports = router;
