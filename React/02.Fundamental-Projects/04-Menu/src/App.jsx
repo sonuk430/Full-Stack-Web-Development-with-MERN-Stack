a721bb474624784859db551764a6d6d578f62fd1
Sonu Kumar <kumarsonuk430@gmail.com>
2026-02-09 20:27:48 +0530

    menu task done

--- React/02.Fundamental-Projects/04-Menu/src/App.jsx
+++ React/02.Fundamental-Projects/04-Menu/src/App.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Title from "./Title";
+import menu from "./data";
+import Menu from "./Menu";
+import Categories from "./Categories";
+
+// const tempCategory = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategory);
+// const tempItem = ["All", ...tempSet];
+
+const allCategories = ["All", ...new Set(menu.map((item) => item.category))];
 
 const App = () => {
+  const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "All") {
+      setMenuItems(menu);
+      return;
+    }
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
     <main>
       <section className="menu">
         <Title text="Our menu" />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
   );
