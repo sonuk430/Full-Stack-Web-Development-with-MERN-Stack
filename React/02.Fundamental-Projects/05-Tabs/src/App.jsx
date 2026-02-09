986d7870c6f75aedda07d8f96f1e00cf6c6f838f
Sonu Kumar <kumarsonuk430@gmail.com>
2026-02-09 21:36:00 +0530

    tab task done

---
+++ React/02.Fundamental-Projects/05-Tabs/src/App.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
+const url = "https://www.course-api.com/react-tabs-project";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  // CurrentItems
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJob = await response.json();
+    setJobs(newJob);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
+};
+export default App;
