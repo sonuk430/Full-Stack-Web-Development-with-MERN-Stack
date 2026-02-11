0a2bb6c5844fb13cd2907641868d47b1ac63cc2f
Sonu Kumar <kumarsonuk430@gmail.com>
2026-02-11 22:09:38 +0530

    todo app done

---
+++ React/02.Fundamental-Projects/07-Grocery-Bud/src/SingleItem.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const SingleItem = ({ item, removeItem, editItem }) => {
+  // const [isDone, setIsDone] = useState(item.completed);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => editItem(item.id)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: item.completed && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        type="button"
+        className="btn remove-btn"
+        onClick={() => {
+          removeItem(item.id);
+        }}
+      >
+        Remove
+      </button>
+    </div>
+  );
+};
+
+export default SingleItem;
